f637ae81989683a6109654bd74edd00d4a5aa335
Alex Roan <alex@cyfrin.io>
2024-02-28 17:09:53 +0000

    Terminal output gets foundry output stdout

--- src/extension.ts
+++ src/extension.ts
@@ -12,25 +12,27 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld" is now active!');
+	let outputChannel = vscode.window.createOutputChannel("Aderyn");
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+
+		outputChannel.show();
+		outputChannel.appendLine("Running aderyn...");
+
 		let path = vscode.workspace.workspaceFolders?.[0].uri.fsPath.toString() ?? "No workspace";
-		vscode.window.showInformationMessage(path);
-		let commandString = "cd " + path + " && pwd && aderyn";
-		vscode.window.showInformationMessage(commandString);
-
-		let outputChannel = vscode.window.createOutputChannel("Aderyn");
-
+		let commandString = "cd " + path + " && pwd && aderyn --output report.json";
 		let cmd = util.promisify(child_process.exec);
-		cmd(commandString).catch((e: any) => vscode.window.showErrorMessage(e.message)).then((result: any) => {
+
+		cmd(commandString)
+		.catch((e: any) => vscode.window.showErrorMessage(e.message))
+		.then((result: any) => {
 			outputChannel.append(result.stdout);
-			outputChannel.show();
 		});
+
 	});
 
 	context.subscriptions.push(disposable);
