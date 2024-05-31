dbc9b07311dd59cc3b0b75252b49e0f3f002e77d
Alex Roan <alex@cyfrin.io>
2024-05-31 17:01:38 +0100

    Using --watch flag

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { spawn, exec } from 'child_process';
+import { spawn, exec, ChildProcess } from 'child_process';
+
+let aderynProcess: ChildProcess | null = null;
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "aderyn-vscode" is now active!');
@@ -18,65 +20,79 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(diagnosticCollection);
 
     let runCommand = vscode.commands.registerCommand('aderyn-vscode.run', () => {
-        const minVersion = '0.1.0';
-        exec('aderyn --version', (error, stdout, stderr) => {
-            if (error) {
-                vscode.window.showErrorMessage(
-                    "Aderyn not found. Please install aderyn.",
-                    "Open Installation Instructions"
-                ).then(selection => {
-                    if (selection === "Open Installation Instructions") {
-                        vscode.env.openExternal(vscode.Uri.parse("https://github.com/Cyfrin/aderyn?tab=readme-ov-file#usage"));
-                    }
-                });
-                return;
-            }
-            const versionPattern = /(\d+\.\d+\.\d+)/; // Regex to extract version number
-            const match = stdout.match(versionPattern);
-            if (match) {
-                const installedVersion = match[1];
-                if (installedVersion >= minVersion) {
-                    diagnosticCollection.clear();
-                    runAderyn(context, aderynOutputChannel, diagnosticCollection);
-                } else {
-                    vscode.window.showErrorMessage(
-                        `Aderyn version is too old. Found: ${installedVersion}, Required: ${minVersion}. Please update aderyn.`,
-                        "Open Installation Instructions"
-                    ).then(selection => {
-                        if (selection === "Open Installation Instructions") {
-                            vscode.env.openExternal(vscode.Uri.parse("https://github.com/Cyfrin/aderyn?tab=readme-ov-file#usage"));
-                        }
-                    });
-                    return;
-                }
-            } else {
-                vscode.window.showErrorMessage(
-                    "Unable to determine the installed version of aderyn.",
-                    "Open Installation Instructions"
-                ).then(selection => {
-                    if (selection === "Open Installation Instructions") {
-                        vscode.env.openExternal(vscode.Uri.parse("https://github.com/Cyfrin/aderyn?tab=readme-ov-file#usage"));
-                    }
-                });
-                return;
-            }
-        });
-    });
-
-    let solidityWatcher = vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-        if (document.languageId === 'solidity' && document.fileName.endsWith('.sol')) {
-            diagnosticCollection.delete(document.uri);
-            runAderyn(context, aderynOutputChannel, diagnosticCollection, document.uri);
+        if (!aderynProcess) {
+            checkAderynVersion(context, aderynOutputChannel, diagnosticCollection);
+        } else {
+            vscode.window.showInformationMessage('Aderyn is already running.');
         }
     });
 
     context.subscriptions.push(runCommand);
+
+    // Add a file save watcher to clear diagnostics
+    let solidityWatcher = vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+        if (document.languageId === 'solidity' && document.fileName.endsWith('.sol')) {
+            diagnosticCollection.clear();
+        }
+    });
+
     context.subscriptions.push(solidityWatcher);
 }
 
-export function deactivate() {}
+export function deactivate() {
+    if (aderynProcess) {
+        aderynProcess.kill();
+        aderynProcess = null;
+    }
+}
 
-function runAderyn(context: vscode.ExtensionContext, aderynOutputChannel: vscode.OutputChannel, diagnosticCollection: vscode.DiagnosticCollection, documentUri?: vscode.Uri) {
+function checkAderynVersion(context: vscode.ExtensionContext, aderynOutputChannel: vscode.OutputChannel, diagnosticCollection: vscode.DiagnosticCollection) {
+    const minVersion = '0.1.0';
+    exec('aderyn --version', (error, stdout, stderr) => {
+        if (error) {
+            vscode.window.showErrorMessage(
+                "Aderyn not found. Please install aderyn.",
+                "Open Installation Instructions"
+            ).then(selection => {
+                if (selection === "Open Installation Instructions") {
+                    vscode.env.openExternal(vscode.Uri.parse("https://github.com/Cyfrin/aderyn?tab=readme-ov-file#usage"));
+                }
+            });
+            return;
+        }
+        const versionPattern = /(\d+\.\d+\.\d+)/; // Regex to extract version number
+        const match = stdout.match(versionPattern);
+        if (match) {
+            const installedVersion = match[1];
+            if (installedVersion >= minVersion) {
+                diagnosticCollection.clear();
+                startAderynWatch(context, aderynOutputChannel, diagnosticCollection);
+            } else {
+                vscode.window.showErrorMessage(
+                    `Aderyn version is too old. Found: ${installedVersion}, Required: ${minVersion}. Please update aderyn.`,
+                    "Open Installation Instructions"
+                ).then(selection => {
+                    if (selection === "Open Installation Instructions") {
+                        vscode.env.openExternal(vscode.Uri.parse("https://github.com/Cyfrin/aderyn?tab=readme-ov-file#usage"));
+                    }
+                });
+                return;
+            }
+        } else {
+            vscode.window.showErrorMessage(
+                "Unable to determine the installed version of aderyn.",
+                "Open Installation Instructions"
+            ).then(selection => {
+                if (selection === "Open Installation Instructions") {
+                    vscode.env.openExternal(vscode.Uri.parse("https://github.com/Cyfrin/aderyn?tab=readme-ov-file#usage"));
+                }
+            });
+            return;
+        }
+    });
+}
+
+function startAderynWatch(context: vscode.ExtensionContext, aderynOutputChannel: vscode.OutputChannel, diagnosticCollection: vscode.DiagnosticCollection) {
     if (!vscode.workspace.workspaceFolders) {
         vscode.window.showErrorMessage("Please open a workspace before running this command.");
         return;
@@ -85,28 +101,26 @@ function runAderyn(context: vscode.ExtensionContext, aderynOutputChannel: vscode
     const workspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
     let stdoutBuffer = '';
 
-    aderynOutputChannel.appendLine("Running aderyn...");
+    aderynOutputChannel.appendLine("Starting Aderyn with --watch...");
 
-    const command = spawn('aderyn', ['--stdout', '-o report.json', '--skip-cloc', '--skip-update-check'], {
+    aderynProcess = spawn('aderyn', ['--watch', '--stdout', '--skip-cloc', '--skip-update-check'], {
         cwd: workspaceFolder,
         shell: true,
     });
 
-    command.stdout.on('data', (data) => {
+    aderynProcess.stdout?.on('data', (data) => {
         stdoutBuffer += data.toString();
         aderynOutputChannel.append(data.toString());
-    });
 
-    command.stderr.on('data', (data) => {
-        aderynOutputChannel.append(data.toString());
-    });
+        const startMarker = stdoutBuffer.indexOf("STDOUT START");
+        const endMarker = stdoutBuffer.indexOf("STDOUT END");
 
-    command.on('close', (code) => {
-        aderynOutputChannel.appendLine(`Aderyn process exited with code ${code}`);
-        if (code === 0) {
+        if (startMarker !== -1 && endMarker !== -1 && endMarker > startMarker) {
+            const reportJsonString = stdoutBuffer.substring(startMarker + "STDOUT START".length, endMarker).trim();
             try {
-                const report = parseStdoutBuffer(stdoutBuffer);
-                highlightIssues(report, context, diagnosticCollection, documentUri);
+                const report = JSON.parse(reportJsonString);
+                diagnosticCollection.clear(); // Clear diagnostics before updating with new data
+                highlightIssues(report, context, diagnosticCollection);
             } catch (error) {
                 vscode.window.showErrorMessage('Error parsing Aderyn output.');
                 if (error instanceof Error) {
@@ -115,37 +129,45 @@ function runAderyn(context: vscode.ExtensionContext, aderynOutputChannel: vscode
                     aderynOutputChannel.appendLine(`Unknown error: ${JSON.stringify(error)}`);
                 }
             }
-        } else {
+            stdoutBuffer = stdoutBuffer.slice(endMarker + "STDOUT END".length);
+        }
+    });
+
+    aderynProcess.stderr?.on('data', (data) => {
+        aderynOutputChannel.append(data.toString());
+    });
+
+    aderynProcess.on('close', (code) => {
+        aderynOutputChannel.appendLine(`Aderyn process exited with code ${code}`);
+        if (code !== 0) {
             vscode.window.showErrorMessage(`Aderyn did not finish successfully. Exit code: ${code}`);
+        }
+        aderynProcess = null;
+    });
+
+    context.subscriptions.push({
+        dispose: () => {
+            if (aderynProcess) {
+                aderynProcess.kill();
+                aderynProcess = null;
+            }
         }
     });
 }
 
-function parseStdoutBuffer(stdoutBuffer: string): any {
-    try {
-        const reportJsonString = stdoutBuffer.split("STDOUT START")[1].split("STDOUT END")[0];
-        return JSON.parse(reportJsonString.trim());
-    } catch (err) {
-        throw new Error('Error parsing JSON from stdout');
-    }
-}
-
-function highlightIssues(report: any, context: vscode.ExtensionContext, diagnosticCollection: vscode.DiagnosticCollection, documentUri?: vscode.Uri) {
+function highlightIssues(report: any, context: vscode.ExtensionContext, diagnosticCollection: vscode.DiagnosticCollection) {
     const issueTypes = ['high_issues', 'low_issues'];
     issueTypes.forEach(type => {
         report[type].issues.forEach((issue: any) => {
             issue.instances.forEach((instance: any) => {
-                highlightIssueInstance(issue, instance, type.toUpperCase(), type === 'high_issues' ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Information, diagnosticCollection, documentUri);
+                highlightIssueInstance(issue, instance, type.toUpperCase(), type === 'high_issues' ? vscode.DiagnosticSeverity.Warning : vscode.DiagnosticSeverity.Information, diagnosticCollection);
             });
         });
     });
 }
 
-function highlightIssueInstance(issue: any, instance: any, severityString: string, diagnosticSeverity: vscode.DiagnosticSeverity, diagnosticCollection: vscode.DiagnosticCollection, documentUri?: vscode.Uri) {
+function highlightIssueInstance(issue: any, instance: any, severityString: string, diagnosticSeverity: vscode.DiagnosticSeverity, diagnosticCollection: vscode.DiagnosticCollection) {
     const issueUri = vscode.Uri.file(path.join(vscode.workspace.workspaceFolders![0].uri.fsPath, instance.contract_path));
-    if (documentUri && documentUri.fsPath !== issueUri.fsPath) {
-        return;
-    }
     const srcParts = instance.src.split(':');
     const startOffset = parseInt(srcParts[0], 10);
     const length = parseInt(srcParts[1], 10);
