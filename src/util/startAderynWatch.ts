2179edf6cddfaa105d6a4495a6ebce30220a1592
unknown <alex.roan@hotmail.com>
2024-06-26 16:34:30 +0100

    Revert "Use temp file to execute watch in bash"
    This reverts commit c28131adf2039621d3183868068099b6cbd9dbe1.

--- src/util/startAderynWatch.ts
+++ src/util/startAderynWatch.ts
@@ -1,10 +1,9 @@
-import * as fs from 'fs';
-import * as os from 'os';
-import * as path from 'path';
-import { getGitBashPath } from './gitBashPath';
 import * as vscode from 'vscode';
 import { spawn, ChildProcess } from 'child_process';
 import { highlightIssues } from '../diagnostics/highlightIssues';
+import * as os from 'os';
+import * as path from 'path';
+import { getGitBashPath } from './gitBashPath';
 
 export function startAderynWatch(
   context: vscode.ExtensionContext,
@@ -35,18 +34,14 @@ export function startAderynWatch(
   const shell = isWindows && gitBashPath ? gitBashPath : true;
 
   const sourceCommands = `
-    if [ -f ~/.zshrc ]; then source ~/.zshrc; fi
-    if [ -f ~/.bashrc ]; then source ~/.bashrc; fi
-    if [ -f ~/.bash_profile ]; then source ~/.bash_profile; fi
-    if [ -f ~/.profile ]; then source ~/.profile; fi
+    [ -f ~/.zshrc ] && source ~/.zshrc;
+    [ -f ~/.bashrc ] && source ~/.bashrc;
+    [ -f ~/.bash_profile ] && source ~/.bash_profile;
+    [ -f ~/.profile ] && source ~/.profile;
     aderyn ${args.join(' ')}
   `;
 
-  // Create a temporary script file
-  const tempScriptPath = path.join(os.tmpdir(), 'run_aderyn.sh');
-  fs.writeFileSync(tempScriptPath, sourceCommands);
-
-  const aderynProcess = spawn('bash', [tempScriptPath], {
+  const aderynProcess = spawn('bash', ['-c', sourceCommands], {
     cwd: workspaceFolder,
     shell,
   });
@@ -82,8 +77,6 @@ export function startAderynWatch(
 
   aderynProcess.on('close', (code) => {
     aderynOutputChannel.appendLine(`Aderyn process exited with code ${code}`);
-    // Remove the temporary script file
-    fs.unlinkSync(tempScriptPath);
   });
 
   return aderynProcess;
