1a87e5804555db66659750b07dd23373fe717903
Alex Roan <alex@cyfrin.io>
2024-06-28 17:34:05 +0100

    Fix tests and bump version

--- src/test/util/checkAderynVersion.test.ts
+++ src/test/util/checkAderynVersion.test.ts
@@ -6,15 +6,17 @@ import proxyquire from 'proxyquire';
 suite('checkAderynVersion Test Suite', () => {
   let sandbox: sinon.SinonSandbox;
   let execStub: sinon.SinonStub;
-  let checkAderynVersion: () => Promise<boolean>;
+  let checkAderynVersion: (aderynOutputChannel: vscode.OutputChannel) => Promise<boolean>;
   let windowShowErrorMessageStub: sinon.SinonStub;
   let envOpenExternalStub: sinon.SinonStub;
+  let aderynOutputChannel: vscode.OutputChannel;
 
   setup(() => {
     sandbox = sinon.createSandbox();
     execStub = sandbox.stub();
     windowShowErrorMessageStub = sandbox.stub(vscode.window, 'showErrorMessage').resolves();
     envOpenExternalStub = sandbox.stub(vscode.env, 'openExternal').resolves();
+    aderynOutputChannel = vscode.window.createOutputChannel('Aderyn');
 
     checkAderynVersion = proxyquire('../../util/checkAderynVersion', {
       'child_process': { exec: execStub },
@@ -26,16 +28,16 @@ suite('checkAderynVersion Test Suite', () => {
   });
 
   test('should return true if version is correct', async () => {
-    execStub.yields(null, 'aderyn 0.1.2', '');
+    execStub.yields(null, 'aderyn 0.1.3', '');
 
-    const result = await checkAderynVersion();
+    const result = await checkAderynVersion(aderynOutputChannel);
     assert.strictEqual(result, true);
   });
 
   test('should return false and show error if aderyn is not found', async () => {
     execStub.yields(new Error('command not found'), '', '');
 
-    const result = await checkAderynVersion();
+    const result = await checkAderynVersion(aderynOutputChannel);
     assert.strictEqual(result, false);
     assert.strictEqual(windowShowErrorMessageStub.calledOnce, true);
     assert.strictEqual(windowShowErrorMessageStub.firstCall.args[0], 'Aderyn not found. Please install aderyn.');
@@ -44,7 +46,7 @@ suite('checkAderynVersion Test Suite', () => {
   test('should return false and show error if version is too old', async () => {
     execStub.yields(null, 'aderyn 0.0.1', '');
 
-    const result = await checkAderynVersion();
+    const result = await checkAderynVersion(aderynOutputChannel);
     assert.strictEqual(result, false);
     assert.strictEqual(windowShowErrorMessageStub.calledOnce, true);
     assert.strictEqual(windowShowErrorMessageStub.firstCall.args[0].includes('Aderyn version is too old.'), true);
